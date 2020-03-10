cb98041626238c526a07abb78593a02c4ef70794
Juan Ocampo <desarrollojuseca@gmail.com>
2020-03-10 12:50:01 -0500

    added gitignore

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - creates nre binary tree
+ * @parent: lparent node
+ * @value: parent int value
+ * Return: i
+*/
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+
+}
