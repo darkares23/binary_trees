010b9ea84afc8d0443c2cf14db3620a445f234ab
Juan Ocampo <desarrollojuseca@gmail.com>
2020-03-10 13:24:17 -0500

    started task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,5 +9,13 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
+	binary_tree_t *new;
 
+	new = malloc(sizeof(binary_tree_t));
+
+	if (!parent);
+		new->n = value;
+		new->left = NULL;
+		new->right = NULL;
+		return new;
 }
