f39123409bd19e4fb4228b4b59953eef0fb0a790
darkares23 <desarrollojuseca@gmail.com>
2020-03-11 20:28:47 -0500

    fixed betty task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -4,6 +4,7 @@
  * binary_tree_insert_left - inserts a node as the left-child of another node
  * @parent: Pointer to the parent node
  * @value: node value
+ * Return: node
  */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
